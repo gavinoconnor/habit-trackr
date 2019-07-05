13a59b522d78d7ae0b6c14a14fd123948a9c3185
gavinoconnor <gedmund@gmail.com>
2019-07-05 17:19:15 -0400

    add front/back end

---
+++ habit-trackr-backend/db/migrate/20190705205036_create_habits.rb
@@ -0,0 +1,10 @@
+class CreateHabits < ActiveRecord::Migration[5.2]
+  def change
+    create_table :habits do |t|
+      t.string :name
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
