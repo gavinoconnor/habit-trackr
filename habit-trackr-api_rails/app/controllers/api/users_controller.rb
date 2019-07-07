d9e1b96a4a34de3d1c4e040b8abbd2abc23ee1df
gavinoconnor <gedmund@gmail.com>
2019-07-07 18:39:29 -0400

    try to fix api folder

---
+++ habit-trackr-api_rails/app/controllers/api/users_controller.rb
@@ -0,0 +1,28 @@
+class Api::UsersController < ApplicationController
+
+  def index
+    users = User.all
+
+    render json: users
+  end
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: user
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(
+      :first_name,
+      :last_name,
+      :username,
+      :email
+    )
+  end
+
+end
