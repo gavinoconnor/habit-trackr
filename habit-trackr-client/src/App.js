990297755a353009008fa5a99a6c13a474f0aa6d
gavinoconnor <gedmund@gmail.com>
2019-07-08 09:18:09 -0400

    add habit page

--- habit-trackr-client/src/App.js
+++ habit-trackr-client/src/App.js
@@ -1,36 +1,26 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import NavBar from './containers/NavBar'
+import Profile from './containers/Profile'
+import Habits from './containers/Habits'
 
 export default class App extends React.Component {
 
   state = {
     users: [],
-    habits: []
-  }
-
-  fetchHabits = () => {
-  fetch("http://localhost:3000/api/habits")
-  .then(res => res.json())
-  .then(data => {
-    this.setState({
-      habits: data,
-      loading: false
-    })
-  })
-}
-
-componentDidMount(){
-    this.fetchHabits()
   }
 
   render() {
-    console.log(this.state.habits)
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is working!</p>
-        <button>Switch Name</button>
-      </div>
+      <Router>
+        <div className="App">
+          <NavBar />
+          <Route exact path="/" render={() => <div>This is your Home page</div>} />
+          <Route path="/profile" component={Profile}/>
+          <Route path="/habits" habits={this.state.habits} component={Habits}/>
+        </div>
+      </Router>
     );
   }
 
